23c0acc8b4a7decb50c9218a55c3f127c5176cf2
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-28 19:44:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// document.addEventListener("contextmenu", function(e) {
-//     e.preventDefault();
-// });
-// document.addEventListener("keydown", function(e) {
-//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+});
