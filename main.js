0f275551401e0153e4e25a662b58202f5fe6bf14
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-28 19:57:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ document.addEventListener("keydown", function(e) {
         e.preventDefault();
     }
 });
+
+
+
+const imageElement = document.querySelector('.image');
+const newImageUrls = [
+  './images/AI.jpg',
+  './images/1.jpg',
+  './images/2.jpg',
+  './images/4.png',
+  './images/kurumi.jpg',
+  './images/kurumiNewY.jpg',
+
+];
+
+function randomizeImage() {
+  const randomImageUrl = newImageUrls[Math.floor(Math.random() * newImageUrls.length)];
+  imageElement.src = randomImageUrl;
+}
+
+window.onload = randomizeImage;
