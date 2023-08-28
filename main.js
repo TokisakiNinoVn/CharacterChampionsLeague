bd4a3a561b513c6fffbf4c6ed827a845a14da3d7
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-28 19:22:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// });
+// document.addEventListener("keydown", function(e) {
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+//         e.preventDefault();
+//     }
+// });
